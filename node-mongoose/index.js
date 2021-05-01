c18dc84b107e92c268f70499eddbd3e873d7b4ac
nick8642 <guptani9926@gmail.com>
2021-05-01 13:32:06 +0530

    mongoose ODM-2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -5,17 +5,14 @@ const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
-
 connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
 
