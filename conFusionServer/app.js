bf8dade0ee9e82368c9802355007f819053377f7
nick8642 <guptani9926@gmail.com>
2021-05-01 19:44:14 +0530

    rest api mongoose part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,13 +14,13 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
+
 const url = 'mongodb://localhost:27017/conFusion';
 
 const connect = mongoose.connect(url);
 connect.then((db)=>{
   console.log('Connected correctly to server');
 },(err)=>{console.log(err);});
-
 
 var app = express();
 
