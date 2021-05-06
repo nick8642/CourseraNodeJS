c59783264b466a25f90a01a48e0cc99de1f2cd15
nick8642 <guptani9926@gmail.com>
2021-05-06 17:37:07 +0530

    cookies

---
+++ conFusionServer/models/leaders.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+},{
+    timestamps: true
+});
+
+var Leaders = mongoose.model('leader',leaderSchema);
+
+module.exports = Leaders;
--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -3,44 +3,58 @@ const bodyParser = require('body-parser');
 
 const promoRouter = express.Router();
 
+const Promotions = require('../models/promotions');
+
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-//first this is called
-.all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
 
-})
-// with same req,res this will be called next
 .get((req,res,next)=>{
-    res.end('Will send all the promotions to you!');
-    
+    Promotions.find({})
+    .then((promotions)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-type','application/json');
+        res.json(promotions);
+    },err=>next(err))
+    .catch((err)=>next(err));   
 })
-// if request is post then after app.all this will be executed
+
 .post((req,res,next)=>{
-    res.end('Will add the promotion: ' + req.body.name  + ' with details: ' + req.body.description);
+    Promotions.create(req.body)
+    .then((promotion)=>{
+        console.log('Promotion Created ',promotion);
+        res.statusCode = 200;
+        res.setHeader('Content-type','application/json');
+        res.json(promotion);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
 .put((req,res,next)=>{
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions ');
 })
 .delete((req,res,next)=>{
-    res.end('Will delete all the promotions!');
-    
+    Promotions.remove({})
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 });
 
 
 
 promoRouter.route('/:promoId')
-.all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 .get((req,res,next)=>{
-    res.end('Will send you details of the promotion: '+ req.params.promoId + ' to you!');
+    Promotions.findById(req.params.promoId)
+    .then((promotion)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+
+    },(err)=>next(err))
+    .then((err)=>next(err));
     
 })
 .post((req,res,next)=>{
@@ -48,12 +62,26 @@ promoRouter.route('/:promoId')
     res.end('POST operation not supported on /promotions/' + req.params.promoId);
 })
 .put((req,res,next)=>{
+    Promotions.findByIdAndUpdate(req.params.promoId,{
+        $set:req.body
+    },
+    { new:true })
+    .then((promotion)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
 
-    res.write('Updating the promotion: '+ req.params.promoId + '\n');
-    res.end('Will update the promotion: '+ req.body.name + ' with details '+ req.body.description);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
 .delete((req,res,next)=>{
-    res.end('Deleting promotion: '+ req.params.promoId);
+    Promotions.findByIdAndRemove(req.params.promoId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
     
 });
 
